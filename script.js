be85d85630916b5228e7b99dbb895932d04199d7
Ethan Irvine <edog.irvine@gmail.com>
2020-01-11 12:32:10 -0700

    displayed the billboard 10 to our html page

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+$(document).ready(function(){
+    billboardFunction();
+})
 
 $('.btn').click(function (event) {
     event.preventDefault();
@@ -71,5 +73,30 @@ function newsAPI(artist){
 
 }
 
+var cdt = moment().format('YYYY-MM-DD');
 
+var billboardlist = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://billboard-api2.p.rapidapi.com/artist-100?date=" + cdt + "&range=1-10",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "billboard-api2.p.rapidapi.com",
+		"x-rapidapi-key": "207e2be657msh6f04971c7f3e427p16486djsn0f3ffe48db38"
+	}
+} || []
+
+$(function billboardFunction() {
+    $.ajax(billboardlist).done(function (response) {
+        console.log(response);
+        console.log(response.content[1].artist);
+        
+        for (let i = 1; i < 11; i++) {
+
+            $('#artistlist').append(
+                '<li class="col-lg-1" id="A">' + response.content[i].artist+ '</li>')
+            }
+        console.log(response.content.length);
+    });
+});
 
