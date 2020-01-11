214850b890f928fcb7aa7213e32c3c260ccfb3b1
jay kottke <jaykottke@cox.net>
2020-01-11 12:01:45 -0700

    staging billboard

--- billboard.js
+++ billboard.js
@@ -24,28 +24,3 @@ $(function() {
 	console.log(response.content.length);
 });
 });
-/*
-$(function() {
-    $("#searchTerm").keypress(function(e){
-        if(e.keyCode===13){
-            var searchTerm = $("#searchTerm").val();
-            var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm + "&format=json&callback=?";
-            $.ajax({
-                url: url,
-                type: 'GET',
-                contentType: "application/json; charsest=utf-8",
-                async: false,
-                dataType: "json",
-                success: function(data, status, jqXHR){
-                    $("#output").html();
-                    for(var i=0; i < data[1].length;i++){
-                        console.log(data);
-                        $("#output").prepend("<div><div class='btn-primary'><a href="
-                        +data[3][i]+"><h1>" + data[1][i]+ "</h1>" + "<p>" + data[2][i] +
-                        "</p></a></div></div>");
-                    }
-    
-                },
-            })
-        }
-    }); */
